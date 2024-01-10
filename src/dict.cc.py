0a13a8d523af4126ad824c379576477f7714aca6
FlavourDave <mail@daviddann.de>
2024-01-10 11:10:09 +0100

    Update script to work with JSON response
    Updates script to return a JSON instead of XML. Also removes the unneeded query print which breaks parsing.

--- src/dict.cc.py
+++ src/dict.cc.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import urllib2, urllib
+import urllib.request, urllib.error, urllib.parse, urllib.request, urllib.parse, urllib.error
 import re
 import sys
 import string
+import json
 
 # Edit here for default number of results
 MAX_RESULTS = 20
@@ -16,9 +17,11 @@ class Dict:
 
     def getResponse(self, word):
         # Trick to avoid dict.cc from denying the request: change User-agent to firefox's
-        req = urllib2.Request("http://www.dict.cc/?s="+word, None, {'User-agent': 'Mozilla/6.0'})
-        f = urllib2.urlopen(req)
-        self.Response = f.read()
+        req = urllib.request.Request(
+            "http://www.dict.cc/?s=" + word, None, {"User-agent": "Mozilla/6.0"}
+        )
+        f = urllib.request.urlopen(req)
+        self.Response = f.read().decode("utf-8")
 
     # Find 'var c1Arr' and 'var c2Arr'
     def parseResponse(self):
@@ -32,22 +35,26 @@ class Dict:
 
         for l in lines:
             if l.find("var c1Arr") >= 0:
-                engLine =  l
+                engLine = l
             elif l.find("var c2Arr") >= 0:
                 deLine = l
 
         if not engLine or not deLine:
             return False
 
-        pattern = "\"[^,]+\""
+        pattern = '"[^,]+"'
 
         # Return list of matching strings
-        self.engWords = map(self.sanitizeWord, re.findall(pattern, engLine))
-        self.deWords = map(self.sanitizeWord, re.findall(pattern, deLine))
+        self.engWords = list(map(self.sanitizeWord, re.findall(pattern, engLine)))
+        self.deWords = list(map(self.sanitizeWord, re.findall(pattern, deLine)))
 
     def getOutputLength(self):
         # Get minumum number of both eng and de
-        minWords = len(self.engWords) if len(self.engWords) <= len(self.deWords) else len(self.deWords)
+        minWords = (
+            len(self.engWords)
+            if len(self.engWords) <= len(self.deWords)
+            else len(self.deWords)
+        )
 
         # Is it more than MAX_RESULTS?
         minWords = minWords if minWords <= MAX_RESULTS else MAX_RESULTS
@@ -59,40 +66,57 @@ class Dict:
 
         return length, minWords
 
-    def printXMLResults(self, expression):
-        print "<?xml version=\"1.0\"?>"
-        print "<items>"
+    def printJsonResults(self, expression):
+        results = []
 
         if not self.engWords or not self.deWords:
-            print "<item valid=\"no\">"
-            print "<title>'%s' not found</title>" % string.strip(expression)
-            print "<icon>de_en.png</icon>"
-            print "</item>"
+            results.append(
+                {
+                    "valid": "no",
+                    "title": f"{string.strip(expression)} not found",
+                    "icon": "de_en.png",
+                }
+            )
 
         else:
             length, minWords = self.getOutputLength()
 
             for word_idx in range(minWords):
-                if self.engWords[word_idx] == "\"\"": continue
-                print "<item valid=\"yes\" arg=\"%s\">" % self.engWords[word_idx]
-                print "<title>%s</title>" % self.engWords[word_idx]
-                print "<subtitle>%s</subtitle>" % self.deWords[word_idx]
-                print "<icon>de_en.png</icon>"
-                print "</item>"
+                if self.engWords[word_idx] == '""':
+                    continue
+                results.append(
+                    {
+                        "valid": "yes",
+                        "arg": self.engWords[word_idx],
+                        "title": self.engWords[word_idx],
+                        "subtitle": self.deWords[word_idx],
+                        "icon": "de_en.png",
+                    }
+                )
+                # print('<item valid="yes" arg="%s">' % self.engWords[word_idx])
+                # print("<title>%s</title>" % self.engWords[word_idx])
+                # print("<subtitle>%s</subtitle>" % self.deWords[word_idx])
+                # print("<icon>de_en.png</icon>")
+                # print("</item>")
 
-        print "</items>"
+        # print("</items>")
+        response = json.dumps({"items": results})
+        sys.stdout.write(response)
 
     def sanitizeWord(self, word):
         word = word.replace("\\", "")
-        return word.strip("\" ")
+        return word.strip('" ')
 
 
 if __name__ == "__main__":
     expression = ""
     for index in range(1, len(sys.argv)):
         expression += sys.argv[index] + " "
+    
+    # print(f"QUERY: {expression}")
+
 
     myDict = Dict()
-    myDict.getResponse(urllib.quote(expression))
+    myDict.getResponse(urllib.parse.quote(expression))
     myDict.parseResponse()
-    myDict.printXMLResults(expression)
+    myDict.printJsonResults(expression)
